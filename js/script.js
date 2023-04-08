72f9b28036bd7cbd8b568a6cb687ffefe9c6e783
tester <testingfellow2@gmail.com>
2023-04-08 17:23:41 +0530

    Added Description to portfolio cards

--- js/script.js
+++ js/script.js
@@ -3,10 +3,48 @@ const header = document.querySelector("header");
 
 menu.addEventListener("click", () => {
     menu.classList.toggle("fa-times");
+    menu.style.color = "#5dabf8";
     header.classList.toggle('toggle')
 })
 
 window.addEventListener("load", () => {
-    menu.classList.remove("fa-times");
-    header.classList.remove('toggle')
+  menu.classList.remove("fa-times");
+  header.classList.remove('toggle')
 })
+
+//  Smooth Scrolling
+const links = document.querySelectorAll(".nav-link");
+
+links.forEach((link) => {
+  link.addEventListener("click", (event) => {
+      event.preventDefault();
+      const target = document.querySelector(link.getAttribute("href"));
+      console.log(target)
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+function scrollToSection(selector) {
+    const target = document.querySelector(selector);
+  target.scrollIntoView({ behavior: "smooth" });
+}
+
+// Scroll to top
+// Select the scroll-to-top button
+const button = document.querySelector('.top');
+
+// Show the button when the user scrolls down
+window.addEventListener('scroll', function() {
+  if (document.documentElement.scrollTop > 0) {
+    button.style.display = 'block';
+  } else {
+    button.style.display = 'none';
+  }
+});
+
+// Smooth scroll to the top of the page when the button is clicked
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+  const target = document.querySelector(button.getAttribute('href'));
+  target.scrollIntoView({ behavior: 'smooth' });
+});
